4a33158c33f0d178b75076674815e833e6605f20
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-12-07 12:04:07 +0200

    Update Kavo.Thread.lua
    updated grasshop

--- Kavo.Thread.lua
+++ Kavo.Thread.lua
@@ -178,6 +178,8 @@ function Kavo.CreateLib(kavName, themeList)
         themeList = themeStyles.Synapse
     elseif themeList == "Serpent" then
         themeList = themeStyles.Serpent
+    elseif themeList == "GrassHop" then
+        themeList = themeStyles.GrassHop
     else
         if themeList.SchemeColor == nil then
             themeList.SchemeColor = Color3.fromRGB(74, 99, 135)
